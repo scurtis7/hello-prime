99b87bda8000f89311429809e134df9581acc5a5
Steve Curtis <steve.curtis@truthmd.com>
2026-05-29 08:05:13 -0400

    add dark mode toggle

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { providePrimeNG } from "primeng/config";
-import Aura from '@primeng/themes/aura'
-// import Material from '@primeng/themes/material'
-// import Lara from '@primeng/themes/lara'
-// import Nora from '@primeng/themes/nora'
+import { MyPreset } from "./mypreset";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,7 +13,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: MyPreset,
+        options: {
+          darkModeSelector: '.my-app-dark',
+        }
       }
     })
   ]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    Button
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element.classList.toggle('my-app-dark');
+  }
 
 }
