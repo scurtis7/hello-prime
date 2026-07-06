dbb07bfc3a0b7b6f95a1fcba5bf14a71bd04dbf4
Steve Curtis <steve.curtis@truthmd.com>
2026-07-06 12:57:59 -0400

    update theme

--- src/app/components/button-comps/button-comps.component.ts
+++ src/app/components/button-comps/button-comps.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-button-comps',
   standalone: true,
-  imports: [],
+  imports: [Button],
   templateUrl: './button-comps.component.html',
   styleUrl: './button-comps.component.scss'
 })
